bc1d3a106e692ac51e690ee8e0fbf19be82d7c6a
Matthew Moore <mmoore@oucs1528.otago.ac.nz>
2018-06-05 11:09:45 +1200

    Added test_main.cpp

---
+++ Test_Main.cpp
@@ -0,0 +1,23 @@
+#include "Integer.h"
+#include <iostream>
+
+using namespace cosc326;
+
+int main() {
+    Integer i("5");
+    Integer j("7");
+    Integer r;
+    std::cout << "Tesing with inputs " << i << " and " << j << '\n';
+    r = i + j;
+    std::cout << "+ " << r << '\n';
+    r = i - j;
+    std::cout << "- " << r << '\n';
+    r = i * j;
+    std::cout << "* " << r << '\n';
+    r = i / j;
+    std::cout << "/ " << r << '\n';
+    Integer a("5");
+    r += a;
+    std::cout << "r + 5 is " << r << '\n';
+
+}
