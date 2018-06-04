3c7fcf08b80c86cae56e9712d64beb1d1cd69871
daanikus <daanikus@gmail.com>
2018-06-04 18:52:09 +1200

    Removed dead code

--- Main.cpp
+++ Main.cpp
@@ -1,11 +1,11 @@
 #include "Integer.h"
 #include <iostream>
-
+#include "Rational.h"
 using namespace cosc326;
 
 int main() {
-    Integer i("5");
-    Integer j("7");
+    Integer i("15");
+    Integer j("20");
     Integer r;
     std::cout << "Tesing with inputs " << i << " and " << j << '\n';
     r = i + j;
@@ -16,8 +16,11 @@ int main() {
     std::cout << "* " << r << '\n';
     r = i / j;
     std::cout << "/ " << r << '\n';
-    Integer a("5");
+    Integer a("-16");
     r += a;
     std::cout << "r + 5 is " << r << '\n';
-
+    r = gcd(i, j);
+    std::cout << "GCD: " << r << '\n';
+    Rational rat(a, r);
+    std::cout << rat;
 }
